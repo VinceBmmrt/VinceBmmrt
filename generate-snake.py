cf5c692fc99c6b2753c1405bdea5c6552d8ff4de
Vincent B <108406366+VinceBmmrt@users.noreply.github.com>
2025-07-25 16:54:04 +0200

    Update generate.py

--- generate-snake.py
+++ generate-snake.py
@@ -1,12 +1,1 @@
-import requests
 
-# Remplace "VinceBmmrt" par ton vrai nom d'utilisateur GitHub
-username = "VinceBmmrt"
-url = f"https://github.com/{username}.github.io/github-contribution-grid-snake/"
-
-response = requests.get(url)
-gif_url = response.json()["url"]
-
-# Sauvegarde le GIF dans le bon répertoire
-with open(".github/snake/github-contribution-grid-snake.svg", "wb") as f:
-    f.write(requests.get(gif_url).content)
